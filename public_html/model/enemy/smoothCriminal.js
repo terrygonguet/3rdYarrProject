13cdacc70117ced4653dfd301b677fa441daa933
X00135511C <X00135511C@computing.stu.it-tallaght.ie>
2017-01-30 14:51:14 +0000

    Continued SmoothCriminal

--- public_html/model/enemy/smoothCriminal.js
+++ public_html/model/enemy/smoothCriminal.js
@@ -6,38 +6,58 @@
 
 class SmoothCriminal extends Enemy {
     
-    constructor (radius, color, health) {
-        super($V([0,0]), radius, color, health);
-        this.path = [];
-        
+    constructor (position, radius, color, health) {
+        super(position, radius, color, health);
+        this.path = [position];
+        this.segments = [];
     }
     
     getLastLine () {
         var line = null;
         if (this.path.length >= 2) {
-            var prev = this.path[this.path.length - 1];
-            line = $L(prev.to, prev.to.subtract(prev.from));
+            var last = this.path[this.path.length - 1];
+            var prev = this.path[this.path.length - 2];
+            line = $L(prev, last.subtract(prev));
         }
         return line;
     }
     
     addPoint (to, speed) {
-        this.path.push(new Segment(this.getLastLine(), to, speed));
+        this.path.push(to);
+        this.segments.push(new Segment(this.getLastLine(), this.path[this.path.length - 1], to, speed));
     }
     
 }
 
 class Segment {
     
-    constructor (line, to, speed) {
-        this.criminal = criminal;
+    constructor (line, from, to, speed) {
+        this.tangent  = line;
+        this.from     = from;
         this.to       = to;
         this.speed    = speed;
+        this.line     = $L(this.from, this.to.subtract(this.from));
         this.time     = 0;
+        this.totalTime= 0;
         this.distance = 0;
-        this.type     = null;
-        this.line     = line;
-        this.from     = this.line.anchor;
+        this.callback = null;
+        
+        if (this.tangent) {
+            this.type = (this.line.isParallelTo(this.tangent) ? "straight" : "curved");
+        } else {
+            this.type = "straight";
+        }
+        
+        switch (this.type) {
+            case "straight" :
+                this.distance = this.to.distanceFrom(this.from);
+                this.totalTime = this.distance / this.speed * 1000;
+                this.callback = function () {
+                    
+                };
+                break;
+        }
+        
     }
     
     getPos (delta) {
