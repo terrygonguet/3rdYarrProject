03fe9db4f54b093f0bd5343c6f124149f506124c
terrygonguet <terry@gonguet.com>
2017-02-08 22:56:18 +0000

    Done 1min level and placeholder boss

--- public_html/levels/lvl1.js
+++ public_html/levels/lvl1.js
@@ -7,23 +7,19 @@
 (function () {
     var time = 2000;
     
-    var dudes = [], max = 0, dude = 0;
+    var dude = null;
     for (var i = 0; i < 6; i++) {
-        var x = Math.floor(Math.random() * shooter.dimensions.e(1));
+        var x = (i + 1) * shooter.dimensions.e(1) / 8;
         dude = new PathingEnemy([
             {position: $V([x, -10]), speed:100},
             {position: $V([x, 150]), speed:0.3},
             {position: $V([x, 149]), speed:150},
-            {position: $V([(x > shooter.dimensions.e(1) / 2 ? shooter.dimensions.e(1) + 10 : -10), 140]), speed:0}
+            {position: $V([(i > 3 ? shooter.dimensions.e(1) + 10 : -10), 140]), speed:0}
         ], 10, "#189", 1);
-        shooter.addEncounter(dude, Math.floor(Math.random() * 3000) + time);
-        dudes.push(dude);
+        shooter.addEncounter(dude, time);
+        time += 500;
     }
-    for (var i of dudes) {
-        var cur = i.getTotalTime();
-        max = (cur > max ? cur : max);
-    }
-    time += 1000 + max;
+    time += 1000 + dude.getTotalTime();
     
     for (var i = 0; i < 5; i++) {
         dude = new SmoothCriminal($V([shooter.dimensions.e(1) / 5, -10]), 10, "#189", 1);
@@ -34,10 +30,10 @@
         time += 500;
         shooter.addEncounter(dude, time);
     }
-    //time += 1000;
+    time += 5000;
     dude = new PathingEnemy([
         {position: $V([4 * shooter.dimensions.e(1) / 5, -10]), speed: 180},
-        {position: $V([4 * shooter.dimensions.e(1) / 5, 200]), speed: 0.2},
+        {position: $V([4 * shooter.dimensions.e(1) / 5, 200]), speed: 0.3},
         {position: $V([4 * shooter.dimensions.e(1) / 5, 201]), speed: 200},
         {position: $V([shooter.dimensions.e(1) + 10, 190]), speed: 0}
     ], 17, "#469", 25);
@@ -53,15 +49,15 @@
         time += 500;
         shooter.addEncounter(dude, time);
     }
-    //time += 1000;
+    time +=500;
     dude = new PathingEnemy([
         {position: $V([shooter.dimensions.e(1) / 5, -10]), speed: 180},
-        {position: $V([shooter.dimensions.e(1) / 5, 200]), speed: 0.2},
+        {position: $V([shooter.dimensions.e(1) / 5, 200]), speed: 0.3},
         {position: $V([shooter.dimensions.e(1) / 5, 201]), speed: 200},
         {position: $V([-10, 190]), speed: 0}
     ], 17, "#469", 25);
     shooter.addEncounter(dude, time);
-    time += dude.getTotalTime();
+    time += dude.getTotalTime() + 1000;
     
     for (var i = 0; i < 3; i++) {
         for (var j = 1; j <= 5; j++) {
@@ -76,31 +72,63 @@
         }
         time += 700;
     }
+    time += dude.getTotalTime();
     
-//    var boss = new Boss($V([100,100]), 20, "#FF7B0F");
-//    boss.addPhase(
-//        new TargetPlayerPattern(boss, 0.5, 1, {radius: 10}),
-//        new Pattern(boss, function (e) {
-//            if (this.enemy.position.distanceFrom(this.dest) <= this.enemy.radius / 2) {
-//                this.dest = (this.dest.eql($V([100,100])) ? $V([500,100]) : $V([100,100]));
-//            }
-//            var dir = this.dest.subtract(this.enemy.position).toUnitVector().x(e.delta / 1000 * this.speed);
-//            this.enemy.position = this.enemy.position.add(dir);
-//        }, {dest: $V([500,100]), speed:100}),
-//        500);
-//        
-//    boss.addPhase(
-//        new TargetPlayerPattern(boss, 1, 0.01, {radius: 10}),
-//        new Pattern(boss, function (e) {
-//            if (this.enemy.position.distanceFrom(this.dests[this.step]) <= this.enemy.radius / 2) {
-//                this.step++;
-//                if (this.step >= this.dests.length) this.step = 0;
-//            }
-//            var dir = this.dests[this.step].subtract(this.enemy.position).toUnitVector().x(e.delta / 1000 * this.speed);
-//            this.enemy.position = this.enemy.position.add(dir);
-//        }, {dests: [$V([500,100]), $V([100,100]), $V([300,300])], speed:100, step: 0}),
-//        500);
-//    shooter.addEncounter(boss, 500);
+    dude = new SmoothCriminal($V([-10, 100]), 17, "#469", 25);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 100]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 200]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 100]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 200]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 100]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) + 10, 100]), 150);
+    shooter.addEncounter(dude, time);
+    
+    dude = new SmoothCriminal($V([shooter.dimensions.e(1) + 10, 200]), 17, "#469", 25);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 200]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 100]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 200]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 100]), 150);
+    dude.addPoint($V([shooter.dimensions.e(1) / 2, 200]), 150);
+    dude.addPoint($V([-10, 200]), 150);
+    shooter.addEncounter(dude, time);
+    time += dude.getTotalTime() + 2000;
+    
+    for (var i = 0; i < 8; i++) {
+        var startpos = $V([(i % 2 ? -10 : shooter.dimensions.e(1) + 10), 2 * shooter.dimensions.e(2) / 3 - i * shooter.dimensions.e(2) / 12]);
+        dude = new SmoothCriminal(startpos, 10, "#924", 1);
+        dude.addPoint(startpos.add($V([(i % 2 ? 1 : -1), 1])), 150);
+        dude.addPoint($V([shooter.dimensions.e(1) / 2, startpos.e(2)]), 150);
+        dude.addPoint($V([shooter.dimensions.e(1) / 2, -10]), 150);
+        shooter.addEncounter(dude, time);
+        time += 300;
+    }
+    time += dude.getTotalTime() + 5000;
+    
+    
+    var boss = new Boss($V([100,100]), 20, "#FF7B0F");
+    boss.addPhase(
+        new TargetPlayerPattern(boss, 0.5, 1, {radius: 10}),
+        new Pattern(boss, function (e) {
+            if (this.enemy.position.distanceFrom(this.dest) <= this.enemy.radius / 2) {
+                this.dest = (this.dest.eql($V([100,100])) ? $V([500,100]) : $V([100,100]));
+            }
+            var dir = this.dest.subtract(this.enemy.position).toUnitVector().x(e.delta / 1000 * this.speed);
+            this.enemy.position = this.enemy.position.add(dir);
+        }, {dest: $V([500,100]), speed:100}),
+        500);
+        
+    boss.addPhase(
+        new TargetPlayerPattern(boss, 1, 0.01, {radius: 10}),
+        new Pattern(boss, function (e) {
+            if (this.enemy.position.distanceFrom(this.dests[this.step]) <= this.enemy.radius / 2) {
+                this.step++;
+                if (this.step >= this.dests.length) this.step = 0;
+            }
+            var dir = this.dests[this.step].subtract(this.enemy.position).toUnitVector().x(e.delta / 1000 * this.speed);
+            this.enemy.position = this.enemy.position.add(dir);
+        }, {dests: [$V([500,100]), $V([100,100]), $V([300,300])], speed:100, step: 0}),
+        500);
+    shooter.addEncounter(boss, time);
 
     console.log(time);
 })();
